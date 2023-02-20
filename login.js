a64a8d6dedf7042081addaff260f0c6f43083a6c
Dishadshetty7 <125894993+Dishadshetty7@users.noreply.github.com>
2023-02-20 21:55:53 +0530

    Create login.js

---
+++ login.js
@@ -0,0 +1,10 @@
+function display(form){
+if (form.username.value=="admin") { 
+		if (form.password.value=="passs") {              
+										window.location.assign('h.html');
+											} else {
+													alert("Invalid Password")
+													}
+								} else {  alert("Invalid Username")
+										}
+						}
